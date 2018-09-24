87ddf718d83f92aa6fcfcfec13ba2d62d4c48e74
Umar Sheikh <umar.m.sheikh@gmail.com>
2018-09-24 21:20:27 +0500

    handle the case when api app is not started

--- product_searcher/app/controllers/home_controller.rb
+++ product_searcher/app/controllers/home_controller.rb
@@ -5,10 +5,15 @@ class HomeController < ApplicationController
     if params[:asin]
       url = URI.parse("http://localhost:3001/products/prices.json?asin=#{params[:asin]}")
       req = Net::HTTP::Get.new(url.to_s)
+      begin
       res = Net::HTTP.start(url.host, url.port) {|http|
         http.request(req)
       }
       @result = JSON.parse(res.body)
+      rescue => e
+        flash[:alert] = "API is not started yet. Please search later."
+        @result = []
+      end
     else
       @result = []
     end
